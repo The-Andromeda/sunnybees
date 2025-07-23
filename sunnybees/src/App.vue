aa066f73ae1b4fd1602e0cf9988263709fe1a367
Percy Eader <megan.eader@us.af.mil>
2025-07-23 16:49:45 -0400

    Tailwindcss added + config

--- sunnybees/src/App.vue
+++ sunnybees/src/App.vue
@@ -1,19 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Dash from './components/Dash.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <div class="wrapper"><Header logo="Sunny" /></div>
   </header>
 
   <main>
-    <TheWelcome />
+    <Dash />
   </main>
 </template>
 
---
+++ sunnybees/src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  logo: string
+}>()
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ logo }}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+</style>
